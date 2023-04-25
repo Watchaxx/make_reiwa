65cc2ddc62ec33eb22277e80909953d80b158d7c
Watchaxx <105423197+Watchaxx@users.noreply.github.com>
2023-04-25 11:45:51 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+using static System.Linq.Enumerable;
+
+internal class Program
+{
+    static void Main()
+    {
+        bool r = false;
+        int N = int.Parse( ReadLine() );
+        string[] K = new string[N];
+        var l = new System.Collections.Generic.List<string>( 1000 );
+
+        foreach( var i in Range( 0, N ) ) {
+            K[i] = ReadLine();
+        }
+        foreach( var i in Range( 0, N ) ) {
+            foreach( var j in Range( 0, N ) ) {
+                if( K[i] + K[j] == "令和" ) {
+                    r = true;
+                }
+                if( l.Count <= 1000 ) {
+                    l.Add( K[i] + K[j] );
+                }
+            }
+        }
+        foreach( var i in Range( 0, l.Count ) ) {
+            WriteLine( $"{l[i]}" );
+        }
+        if( r == true ) {
+            WriteLine( "Nice" );
+        } else {
+            WriteLine( "Bad" );
+        }
+        return;
+    }
+}
